ce5245063431da52dfc0cafb2893cfb22abc2981
Anton Tokar <tosha.tokar16@gmail.com>
2017-02-20 18:20:49 +0200

    #16 orders new fields

--- src/Eshop/ShopBundle/Form/Type/OrdersType.php
+++ src/Eshop/ShopBundle/Form/Type/OrdersType.php
@@ -2,7 +2,9 @@
 
 namespace Eshop\ShopBundle\Form\Type;
 
+use Eshop\ShopBundle\Entity\Orders;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -25,6 +27,35 @@ class OrdersType extends AbstractType
             ->add('comment', TextareaType::class, array(
                 'required' => false
             ))
+            ->add('deliveryType', ChoiceType::class, [
+                'required' => false,
+                'multiple' => false,
+                'choices' => [
+                    'Самовывоз' => Orders::DELIVERY_TYPE_SELF_PICKUP,
+                    'Новая почта' => Orders::DELIVERY_TYPE_NOVA_POSHTA,
+                ],
+            ])
+            ->add('paymentType', ChoiceType::class, [
+                'required' => false,
+                'multiple' => false,
+                'choices' => [
+                    'Наложенным платежом' => Orders::PAYMENT_TYPE_CASH_ON_DELIVERY,
+                    'Наличными курьеру' => Orders::PAYMENT_TYPE_CASH_COURIERS,
+                    'Банковской картой' => Orders::PAYMENT_TYPE_CREDIT_CARD,
+                    'Оплатить по счёту' => Orders::PAYMENT_TYPE_PAY_ON_ACCOUNT,
+                ],
+            ])
+            ->add('status', ChoiceType::class, [
+                'required' => false,
+                'multiple' => false,
+                'choices' => [
+                    'Новый' => Orders::ORDER_STATUS_NEW,
+                    'В обработке' => Orders::ORDER_STATUS_IN_PROGRESS,
+                    'Отправлен (отправлено по НП)' => Orders::ORDER_STATUS_SEND,
+                    'Завершен (получены деньги)' => Orders::ORDER_STATUS_RECEIVED,
+                ],
+            ])
+            ->add('city', TextType::class)
         ;
     }
 
